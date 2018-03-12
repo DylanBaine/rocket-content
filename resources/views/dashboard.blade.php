f4073d8611f887c15773d4b4d6c75a37297837d8
Dylan Baine <dylan.baine@yahoo.com>
2018-03-12 12:28:36 -0500

    rewrite all requests to https

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,50 @@
 	</section>
 	<section>
 		<header class="padded">
-			<h1 class="bigger">Settings</h1>
+			<h1 class="bigger">Sitewide Settings</h1>
+		</header>
+		<section>
+			<v-layout class="flex-center text-xs-center" row wrap>
+				<v-flex md6 xs10 offset-md3 offset-xs1>
+					<form action="{{route('user.edit')}}" method="post">
+						<v-card class="text-xs-center padded-lg">
+
+							{{csrf_field()}}
+							{{method_field('put')}}
+
+							<div class="margin-auto" style="width: 80%;">
+								<v-text-field
+									name="email"
+									label="Email"
+									value="{{Auth::user()->email}}"
+								></v-text-field>
+								<v-text-field
+									name="name"
+									label="name"
+									value="{{Auth::user()->name}}"
+								></v-text-field>
+
+								<v-text-field
+									name="password"
+									type="password"
+									label="New Password"
+								></v-text-field>
+							</div>
+
+							<v-card-actions>
+								<v-btn type="submit" color="primary" block>Save</v-btn>
+							</v-card-actions>                        
+							
+
+						</v-card>
+					</form>
+				</v-flex>
+			</v-layout>			
+		</section>
+	</section>
+	<section>
+		<header class="padded">
+			<h1 class="bigger">Authentication Settings</h1>
 		</header>
 		<section>
 			<v-layout class="flex-center text-xs-center" row wrap>
